da2df5ebeb3cfb3b356e943f7ecfb3bc3131dbcb
indiciez <justinmajura@gmail.com>
2022-08-01 01:50:46 +0300

    mysql database configuration ... get requests

--- controller/controller.js
+++ controller/controller.js
@@ -2,7 +2,14 @@ const Modal = require('../modal/modal')
 
 exports.readAllItems = (req,res) => {
     Modal.fetchItems((err,data) =>{
-        if(err) res.status(500).send({error:'Error from controller'})
+        if(err) res.status(500).send({error:'Error from controller 1'})
+        else res.send(data);
+    })
+}
+
+exports.readSingleItems = (req,res) => {
+    Modal.fetchSingleItems(req.params.id,(err,data) =>{
+        if(err) res.status(500).send({error:'Error from controller 2'})
         else res.send(data);
     })
 }
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,6 @@ const Controller = require('../controller/controller')
 
 router.get('/',(Controller.readAllItems));
 router.get('/items',(Controller.readAllItems));
+router.get('/items/:id',Controller.readSingleItems);
 
 module.exports = router;
